de30c947e4f3964a1f52fdf186418bf6905c8af9
John <john.19col@gmail.com>
2019-11-25 18:35:10 -0500

    Move types to proper location (src root)

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { RequestWithBody } from './types/express';
+import { RequestWithBody } from './../types/express';
 import { credentialsCorrect } from './../login';
 
 const router = Router();
