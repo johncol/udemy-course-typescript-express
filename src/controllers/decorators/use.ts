e269c507a29098f9590a54676ee1b9e962b50fda
John <john.19col@gmail.com>
2019-11-28 13:11:50 -0500

    Middleware type as alias of express RequestHandler

--- src/controllers/decorators/use.ts
+++ src/controllers/decorators/use.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
-import { Request, Response, NextFunction } from 'express';
+import { RequestHandler } from 'express';
 import { Metadata } from './enums/Metadata';
 
-export type Middleware = (request: Request, response: Response, next: NextFunction) => void;
+export type Middleware = RequestHandler;
 
 export const Use = (...newMiddlewares: Middleware[]) => {
   return (target: any, key: string, descriptor: PropertyDescriptor) => {
